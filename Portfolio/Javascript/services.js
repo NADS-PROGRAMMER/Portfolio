44406b2fc83e7e2404cde2845578cfb5f6869a48
Adrian Nads <marcelonads@gmail.com>
2021-07-22 23:45:53 +0800

    First Commit

---
+++ Portfolio/Javascript/services.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+function showImage(source, serviceTitle) {
+
+    let image = document.getElementById('image-service');
+    let title = document.getElementById('service-title');
+
+    image.src = source;
+    title.textContent = serviceTitle;
+}
+
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    let image = document.getElementById('image-service');
+    let firstImage = document.getElementById('first-image');
+    let secondImage = document.getElementById('second-image');
+    let thirdImage = document.getElementById('third-image');
+
+    firstImage.addEventListener('click', function() {
+
+        showImage('\/Portfolio\/Javascript\/Services Image\/Web 1920 – 3.jpg', 'E-Commerce Site');
+    });
+
+    secondImage.addEventListener('click', function() {
+
+        showImage('\/Portfolio\/Javascript\/Services Image\/Product Management System.png', 'Product Management System');
+    });
+
+    thirdImage.addEventListener('click', function() {
+
+        showImage('\/Portfolio\/Javascript\/Services Image\/Animate 1.jpg', 'Web Design');
+    })
+})
