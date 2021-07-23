d06b35823a28d8ce3f6ef2a2cd523aeb2320bd4f
Adrian Nads <marcelonads@gmail.com>
2021-07-23 13:01:34 +0800

    Change the source of the image in javascript services file

--- Portfolio/Javascript/services.js
+++ Portfolio/Javascript/services.js
@@ -20,16 +20,16 @@ window.addEventListener('DOMContentLoaded', function() {
 
     firstImage.addEventListener('click', function() {
 
-        showImage('\/Portfolio\/Javascript\/Services Image\/Web 1920 – 3.jpg', 'E-Commerce Site');
+        showImage('Portfolio\/Javascript\/Services Image\/Web 1920 – 3.jpg', 'E-Commerce Site');
     });
 
     secondImage.addEventListener('click', function() {
 
-        showImage('\/Portfolio\/Javascript\/Services Image\/Product Management System.png', 'Product Management System');
+        showImage('Portfolio\/Javascript\/Services Image\/Product Management System.png', 'Product Management System');
     });
 
     thirdImage.addEventListener('click', function() {
 
-        showImage('\/Portfolio\/Javascript\/Services Image\/Animate 1.jpg', 'Web Design');
+        showImage('Portfolio\/Javascript\/Services Image\/Animate 1.jpg', 'Web Design');
     })
 })
